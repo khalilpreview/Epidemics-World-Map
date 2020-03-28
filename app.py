f993743b4a71f480362496bdc776535e9a726a66
khalilpreview <previewsites40@gmail.com>
2020-03-28 21:48:55 +0100

    seconde commit

--- app.py
+++ app.py
@@ -2,30 +2,31 @@ import os
 import datetime
 from flask import Flask , render_template , redirect , url_for
 
+# Flask app initialisation
 app = Flask(__name__)
 
 
 # routes and views start here.
-
+# ...
 # home the index route and view
 @app.route('/')
 def home_page():
     title = 'E-W-M'
     return render_template('index.html' , title=title)
 
-
+# registration  route and view
 @app.route('/register')
 def register_page():
     title = 'Registration E-W-M'
     return render_template('registration.html' , title=title)
 
-
+# login route and view
 @app.route('/login')
 def registeration_page():
     title = 'Login E-W-M'
     return render_template('login.html' , title=title)
 
-
+# dashboard route and view
 @app.route('/dashboard')
 def dashboard_page():
     title = 'Dashboard E-W-M'
