f5f109409e4b99fd1816f6f95577f2a5251a070e
khalilpreview <previewsites40@gmail.com>
2020-03-28 21:19:33 +0100

    routes setting

--- app.py
+++ app.py
@@ -5,7 +5,28 @@ from flask import Flask , render_template , redirect , url_for
 app = Flask(__name__)
 
 
+# routes and views start here.
+
+# home the index route and view
 @app.route('/')
-def hello_world():
+def home_page():
     title = 'E-W-M'
     return render_template('index.html' , title=title)
+
+
+@app.route('/register')
+def register_page():
+    title = 'Registration E-W-M'
+    return render_template('registration.html' , title=title)
+
+
+@app.route('/login')
+def registeration_page():
+    title = 'Login E-W-M'
+    return render_template('login.html' , title=title)
+
+
+@app.route('/dashboard')
+def dashboard_page():
+    title = 'Dashboard E-W-M'
+    return render_template('dashboard.html' , title=title)
