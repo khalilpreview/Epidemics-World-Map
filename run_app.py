c227c976431292ac7c96a3190f2c165e68f93fc7
khalilpreview <previewsites40@gmail.com>
2020-04-02 00:23:47 +0100

    version 1.0

--- run_app.py
+++ run_app.py
@@ -10,6 +10,6 @@ from app import app
 
 if __name__ == '__main__':
     app.debug = True
-    host = os.environ.get('IP', '192.168.43.68')
+    host = os.environ.get('IP', '192.168.1.8')
     port = int(os.environ.get('PORT', 8880))
     app.run(host=host, port=port)
